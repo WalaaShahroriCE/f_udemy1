367f2371780c1be12731e07f70ab11ba38354307
WalaaShahrori <shahroriw@gmail.com>
2023-02-07 20:17:40 +0200

    TrialApp

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -5,12 +5,15 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pink,
+        backgroundColor: Color.fromARGB(255, 11, 143, 172),
+        centerTitle: true,
+        elevation: 20.0,
         leading: Icon(
           Icons.menu,
         ),
         title: Text(
-          'Home',
+          'اعرف بلدك',
+          style: TextStyle(fontSize: 24),
         ),
         actions: [
           IconButton(
@@ -24,42 +27,143 @@ class HomeScreen extends StatelessWidget {
                 print("start search");
               },
               icon: Icon(Icons.search)),
-          IconButton(
-            icon: Icon(Icons.access_alarm),
-            onPressed: () {
-              print("alarming");
-            },
-          ),
         ],
-        centerTitle: true,
-        elevation: 20.0,
       ),
-      body: Column(children: [
-        Container(
-          height: 100.0,
-          color: Colors.green,
-          child: Text(
-            "Nablus",
-            style: TextStyle(fontSize: 30.0, color: Colors.black),
-          ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.black,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 40.0, right: 10.0, left: 10.0),
+                    child: Container(
+                        height: 100,
+                        color: Colors.orange,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: Text(
+                            "نابلس",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        )),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 40.0, right: 10.0, left: 10.0),
+                    child: Container(
+                        height: 100,
+                        color: Colors.green,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: Text(
+                            "رام الله",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, right: 10.0, left: 10.0),
+                    child: Container(
+                        height: 100,
+                        color: Colors.yellow,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: Text(
+                            "جنين",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        )),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, right: 10.0, left: 10.0),
+                    child: Container(
+                        height: 100,
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: Text(
+                            "طولكرم",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, right: 10.0, left: 10.0),
+                    child: Container(
+                      height: 50,
+                      color: Color.fromARGB(255, 223, 222, 210),
+                      child: Text(
+                        "موطني...موطني",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(top: 30.0, right: 10.0, left: 10.0),
+                child: Container(
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Text(
+                    " موقع الفلكي: تقع فلسطين التاريخية بين دائرتي عرض 29.30 ـ 33.15 شمالاً ",
+                    maxLines: 4,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-        Container(
-          height: 100.0,
-          color: Colors.blue,
-          child: Text(
-            "Ramallah",
-            style: TextStyle(fontSize: 30.0, color: Colors.black),
-          ),
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.grey,
-          child: Text(
-            "Jenin",
-            style: TextStyle(fontSize: 30.0, color: Colors.black),
-          ),
-        ),
-      ]),
+      ),
     );
   }
 }
